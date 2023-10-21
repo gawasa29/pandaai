94fad13915ef0f5dd00388a1f178f963aa7d330e
Me is gawasa <yusei112929@gmail.com>
2023-10-21 23:14:55 +0900

    start pandaai

---
+++ train.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+from ultralytics import settings
+# Update a setting
+settings.update({'datasets_dir': '.'})
+
+# Load a pre-trained YOLO model
+model = YOLO("yolov8l.pt")
+
+# 'pandaai.yaml' dataset to train the model
+model.train(data="pandaai.yaml", epochs=100, batch=8, workers=4, degrees=90.0)
