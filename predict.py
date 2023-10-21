0180d389cc59a327f367cfb710289fae2c33f875
Me is gawasa <yusei112929@gmail.com>
2023-10-21 23:40:02 +0900

    fix code

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Model Load
-model = YOLO('./runs/detect/train15/weights/last.pt')
+model = YOLO('./runs/detect/train/weights/last.pt')
 
 # Model Use
 model.predict('https://cdn.i-scmp.com/sites/default/files/d8/images/canvas/2022/01/26/795b5720-0a91-45cd-8f0c-ccfe494b1836_9d1e5b38.jpg', save=True, conf=0.2, iou=0.5)
--- train.py
+++ train.py
@@ -7,4 +7,4 @@ settings.update({'datasets_dir': '.'})
 model = YOLO("yolov8l.pt")
 
 # 'pandaai.yaml' dataset to train the model
-model.train(data="pandaai.yaml", epochs=100, batch=8, workers=4, degrees=90.0)
+model.train(data="pandaai.yaml", epochs=300, batch=8, workers=4, degrees=90.0)
