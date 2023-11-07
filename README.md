605b730b16080fdbceaf0fb2341586296175bd5d
Me is gawasa <yusei112929@gmail.com>
2023-11-07 21:31:38 +0900

    update

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ Usage
 -----
 
 ```
+python train.py
+python predict.py
 ```
 
 Implementation
 -----
+pandaai is a panda detection program using YOLOv8.
+It is not yet complete due to lack of image data.
--- train.py
+++ train.py
@@ -5,4 +5,4 @@ model = YOLO("yolov8l.pt")
 
 # 'pandaai.yaml' dataset to train the model
 #  mps is Apple M1 use
-model.train(data="pandaai.yaml", epochs=300, batch=32, device='mps')
+model.train(data="pandaai.yaml", epochs=100, batch=32, device='mps')
